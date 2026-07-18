d3e48a69b7c374fe1658c74ae278d35da713cdf8
efgdigitalid <zulhoiri@gmail.com>
2026-07-18 14:48:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-console.log("IconMotion Studio AI");
+let templates = {};
+let motions = [];
+
 fetch("./templates.json")
-    .then(res => res.json())
+    .then(response => response.json())
     .then(data => {
-        console.log("Templates:", data);
+        templates = data;
+        console.log("Templates loaded:", templates);
+    })
+    .catch(error => {
+        console.error("Error loading templates:", error);
     });
 
 fetch("./motions.json")
-    .then(res => res.json())
+    .then(response => response.json())
     .then(data => {
-        console.log("Motions:", data);
+        motions = data;
+        console.log("Motions loaded:", motions);
+    })
+    .catch(error => {
+        console.error("Error loading motions:", error);
     });
+
+console.log("IconMotion Studio AI v1.1");
