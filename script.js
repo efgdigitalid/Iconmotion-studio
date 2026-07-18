75703dd3f48088123dfc2b3d728072a47d1fea9f
efgdigitalid <zulhoiri@gmail.com>
2026-07-18 14:46:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("IconMotion Studio AI");
+fetch("./templates.json")
+    .then(res => res.json())
+    .then(data => {
+        console.log("Templates:", data);
+    });
+
+fetch("./motions.json")
+    .then(res => res.json())
+    .then(data => {
+        console.log("Motions:", data);
+    });
